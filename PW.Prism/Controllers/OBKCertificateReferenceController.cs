f9f48f2b6674030b65e52a6cfb38242cd6278d3a
Agyn <agyn.bolatov.1992@gmail.com>
2017-11-16 12:55:52 +0600

    Справочник сертификатов

---
+++ PW.Prism/Controllers/OBKCertificateReferenceController.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Script.Serialization;
+using Antlr.Runtime;
+using Aspose.Pdf;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.Infrastructure;
+using Kendo.Mvc.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PW.Ncels.Database.Constants;
+using PW.Ncels.Database.Helpers;
+using PW.Ncels.Database.Models;
+using PW.Ncels.Database.DataModel;
+using PW.Ncels.Database.Repository.Common;
+using PW.Ncels.Database.Repository.Expertise;
+using Document = PW.Ncels.Database.DataModel.Document;
+
+namespace PW.Prism.Controllers
+{
+    [Authorize]
+    public class OBKCertificateReferenceController : Controller
+    {
+        private ncelsEntities db = UserHelper.GetCn();
+
+        // GET: /Reference/
+        public ActionResult Index()
+        {
+            Guid guid = Guid.NewGuid();
+            return PartialView(guid);
+        }
+
+
+        public ActionResult AllList([DataSourceRequest] DataSourceRequest request, string status)
+        {
+            var list = db.OBK_CertificateReference.ToList();
+            foreach (var obj in list)
+            {
+                if (obj.EndDate < DateTime.Now)
+                {
+                    OBK_CertificateReference d = db.OBK_CertificateReference.First(o => o.Id == obj.Id);
+                    d.OBK_CertificateValidityType = db.OBK_CertificateValidityType.First(o => o.Code == "Passive");
+                    db.SaveChanges();
+                }
+
+            }
+
+            var data = from certRef in db.OBK_CertificateReference
+                       join dic in db.Dictionaries on certRef.CertificateCountryId equals dic.Id
+                       join certType in db.OBK_Ref_CertificateType on certRef.CertificateTypeId equals certType.Id
+                       join validityType in db.OBK_CertificateValidityType on certRef.CertificateValidityTypeId equals validityType.Id
+                       select new
+                       {
+                           Id = certRef.Id,
+                           Number = certRef.Number,
+                           CertificateNumber = certRef.CertificateNumber,
+                           StartDate = certRef.StartDate,
+                           EndDate = certRef.EndDate,
+                           country = dic.NameKz,
+                           CertificateOrganization = certRef.CertificateOrganization,
+                           CertificateType = certType.NameRu,
+                           CertificateTypeId = certRef.CertificateTypeId,
+                           CertificateCountryId = certRef.CertificateCountryId,
+                           CertificateCountry = dic.Name,
+                           LastInspection = certRef.LastInspection,
+                           DocumentId = certRef.DocumentId,
+                           CertificateValidityType = validityType.Name,
+                           CertificateValidityCode = validityType.Code
+                       };
+
+            if (status.Equals("Active"))
+            {
+                return Json(data.ToList().Where(d => d.CertificateValidityCode == "Active").ToDataSourceResult(request));
+            }
+            else if (status.Equals("Passive"))
+            {
+                return Json(data.ToList().Where(d => d.CertificateValidityCode == "Passive").ToDataSourceResult(request));
+            }
+            else if (status.Equals("Recalled"))
+            {
+                return Json(data.ToList().Where(d => d.CertificateValidityCode == "Recalled").ToDataSourceResult(request));
+            }
+
+            return Json(data.ToDataSourceResult(request));
+        }
+
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult DicCreate([DataSourceRequest] DataSourceRequest request, OBKCertificateReferenceModel dictionary,
+            string type)
+        {
+
+            if (dictionary != null && ModelState.IsValid)
+            {
+
+                OBK_CertificateReference d = new OBK_CertificateReference()
+                {
+                    Id = Guid.NewGuid(),
+                    Number = dictionary.Number,
+                    CertificateNumber = dictionary.CertificateNumber,
+                    StartDate = dictionary.StartDate,
+                    EndDate = dictionary.EndDate,
+                    CertificateCountryId = dictionary.CertificateCountryId,
+                    CertificateOrganization = dictionary.CertificateOrganization,
+                    CertificateTypeId = dictionary.CertificateTypeId,
+                    LastInspection = dictionary.LastInspection,
+                    DocumentId = dictionary.DocumentId,
+                };
+                if (dictionary.EndDate >= DateTime.Now)
+                {
+                    OBK_CertificateValidityType t = db.OBK_CertificateValidityType.First(o => o.Code == "active");
+                    d.OBK_CertificateValidityType = t;
+                }
+                else
+                {
+                    OBK_CertificateValidityType t = db.OBK_CertificateValidityType.First(o => o.Code == "passive");
+                    d.OBK_CertificateValidityType = t;
+                }
+
+                db.OBK_CertificateReference.Add(d);
+                db.SaveChanges();
+                dictionary.Id = d.Id;
+            }
+
+            return Json(new[] { dictionary }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult DicUpdate([DataSourceRequest] DataSourceRequest request, OBKCertificateReferenceModel dictionary,
+            string type)
+        {
+            if (dictionary != null && ModelState.IsValid)
+            {
+                OBK_CertificateReference d = db.OBK_CertificateReference.First(o => o.Id == dictionary.Id);
+                d.Number = dictionary.Number;
+                d.CertificateNumber = dictionary.CertificateNumber;
+                d.StartDate = dictionary.StartDate;
+                d.EndDate = dictionary.EndDate;
+                d.CertificateCountryId = dictionary.CertificateCountryId;
+                d.CertificateOrganization = dictionary.CertificateOrganization;
+                d.CertificateTypeId = dictionary.CertificateTypeId;
+                d.LastInspection = dictionary.LastInspection;
+                d.DocumentId = dictionary.DocumentId;
+                db.SaveChanges();
+            }
+
+            return Json(new[] { dictionary }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult DicDestroy([DataSourceRequest] DataSourceRequest request, OBKCertificateReferenceModel dictionary,
+            string type)
+        {
+            if (dictionary != null)
+            {
+                OBK_CertificateReference d = db.OBK_CertificateReference.First(o => o.Id == dictionary.Id);
+                Document doc = db.Documents.First(o => o.Id == d.DocumentId);
+
+                db.OBK_CertificateReference.Remove(d);
+                db.SaveChanges();
+            }
+
+            return Json(new[] { dictionary }.ToDataSourceResult(request, ModelState));
+        }
+
+        public ActionResult CountryList()
+        {
+            var data = db.Dictionaries.Where(c => c.Type == "Country").Select(o => new { Id = o.Id, Name = o.Name }).OrderBy(x => x.Name).ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult TypeList()
+        {
+            var data = db.OBK_Ref_CertificateType.Select(o => new { Id = o.Id, Name = o.NameRu }).OrderBy(x => x.Name).ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DocumentRead(Guid id)
+        {
+            Document document = db.Documents.Find(id);
+            if (document == null)
+            {
+                document = new Document()
+                {
+                    Id = id,
+                    DocumentType = 1,
+                    DocumentDate = DateTime.Now,
+                    OutgoingType = 0,
+                    AttachPath = FileHelper.GetObjectPathRoot()
+
+                };
+            }
+
+            DocumentModel model = new DocumentModel(document);
+            return Content(JsonConvert.SerializeObject(model, Formatting.Indented, new JsonSerializerSettings() { DateFormatString = "dd.MM.yyyy HH:mm" }));
+
+        }
+
+        [HttpPost]
+        public ActionResult DocumentUpdate(DocumentModel document)
+        {
+            if (document != null)
+            {
+
+                Document baseDocument = db.Documents.Find(document.Id);
+                if (baseDocument == null)
+                {
+                    baseDocument = new Document()
+                    {
+                        Id = document.Id,
+                        DocumentType = 1,
+                        CreatedDate = DateTime.Now,
+                        ModifiedDate = DateTime.Now,
+                        DocumentDate = DateTime.Now,
+                    };
+                    db.Documents.Add(baseDocument);
+                }
+                baseDocument = document.GetDocument(baseDocument);
+
+                db.SaveChanges();
+                return Content(bool.TrueString);
+            }
+
+            return Content(bool.FalseString);
+        }
+
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Recall([DataSourceRequest] DataSourceRequest request, Guid? Id)
+        {
+            OBK_CertificateReference obj = db.OBK_CertificateReference.FirstOrDefault(o => o.Id == Id);
+            if (obj != null)
+            {
+                obj.OBK_CertificateValidityType = db.OBK_CertificateValidityType.First(o => o.Code == "Recalled");
+                db.SaveChanges();
+                return Json(new
+                {
+                    success = true
+                });
+            }
+
+            return Json(new
+            {
+                success = false
+            });
+
+        }
+    }
+}
---
+++ PW.Prism/Scripts/certificateReference.js
@@ -0,0 +1,267 @@
+﻿
+function InitializeCertificate(name) {
+    var validator = $("#outDocForm" + name).kendoValidator().data("kendoValidator");
+
+    var viewModel = kendo.observable({
+        document: {},
+        change: function () {
+            validator.validate();
+            this.set("hasChanges", true);
+        },
+        initButton: function () {
+
+            var state = this.get("document.StateType");
+            if (state == 0) {
+                this.set("hasRegister", true);
+            } else {
+                this.set("hasRegister", false);
+            }
+            if (state == 1) {
+                this.set("hasOnjob", true);
+            } else {
+                this.set("hasOnjob", false);
+            }
+            if (state > 1 && state < 9) {
+                this.set("hasExecute", true);
+            } else {
+                this.set("hasExecute", false);
+            }
+            if (state > 0) {
+                this.set("hasPrint", true);
+            } else {
+                this.set("hasPrint", false);
+            }
+        },
+        hasChanges: false,
+        hasRegister: false,
+        register: function (e) {
+            e.preventDefault();
+            if (validator.validate()) {
+                this.set("hasChanges", false);
+                registerData();
+            };
+
+        },
+        hasOnjob: false,
+        onjob: function (e) {
+            e.preventDefault();
+            sendDoc();
+        },
+        hasPrint: false,
+        print: function (e) {
+            e.preventDefault();
+            PrintDocumetnt(this.get("document.Id"));
+        },
+        deleteDoc: function (e) {
+            e.preventDefault();
+            DeleteDocumetnt(this.get("document.Id"));
+        },
+        dictionaryView: function (e) {
+            e.preventDefault();
+            DictionaryView(name, 'True', this);
+        },
+        save: function (e) {
+            e.preventDefault();
+            this.set("hasChanges", false);
+            sendData();
+        }
+    });
+
+    function loadDocument() {
+        kendo.ui.progress($('#loader' + name), true);
+        $.ajax({
+            type: 'get',
+            url: '/OBKCertificateReference/DocumentRead?Id=' + name,
+            contentType: 'application/json; charset=utf-8',
+            success: function (result) {
+                result = JSON.parse(result);
+                // alert(JSON.stringify(result));
+                viewModel.set("document", result);
+                viewModel.initButton();
+                //viewModel.person = JSON.stringify(result);
+
+                InitializePropertyCertificate(name, viewModel);
+                kendo.bind($("#outDocForm" + name), viewModel);
+                kendo.ui.progress($('#loader' + name), false);
+            },
+            complete: function () {
+				//InitializeStatusBar(name, viewModel);
+            }
+        });
+    }
+    function sendDoc() {
+        var window = $("#TaskCommandWindow");
+        window.kendoWindow({
+            width: "550px",
+            height: "auto",
+            modal: true, resizable: false,
+            close: onCloseCommandWindow,
+            title: 'Резолюция',
+            actions: ["Close"]
+        });
+
+        window.data("kendoWindow").title('Отправка документа');
+        window.data("kendoWindow").setOptions({
+            width: 550,
+            height: 'auto'
+        });
+        window.data("kendoWindow").refresh('/OutgoingDoc/DocumentSend?id=' + viewModel.get("document.Id"));
+
+        window.data("kendoWindow").center();
+        window.data("kendoWindow").open();
+
+        $("#TaskCommandWindow").closest(".k-window").css({
+            top: 55,
+        
+        });
+    }
+    function registerData() {
+        kendo.ui.progress($('#loader' + name), true);
+
+        var json = JSON.stringify(viewModel.get('document'));
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/OutgoingDoc/DocumentRegister',
+            data: json,
+            contentType: 'application/json; charset=utf-8',
+            success: function (result) {
+                result = JSON.parse(result);
+                if (result.State == true) {
+                    viewModel.set("document", result.document);
+                    viewModel.initButton();
+                    CardRegisterSuccess(result.document.Number, result.document.DocumentDate);
+                };
+            },
+            complete: function () {
+                kendo.ui.progress($('#loader' + name), false);
+            }
+        });
+    };
+
+    function reviewData() {
+        kendo.ui.progress($('#loader' + name), true);
+
+        var json = JSON.stringify(viewModel.get('document'));
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/OutgoingDoc/DocumentSend',
+            data: json,
+            contentType: 'application/json; charset=utf-8',
+            success: function (result) {
+              //  result = JSON.parse(result);
+                if (result == 'True') {
+
+                    CardReviewSuccess();
+                };
+            },
+            complete: function () {
+                kendo.ui.progress($('#loader' + name), false);
+            }
+        });
+    };
+    function sendData() {
+        kendo.ui.progress($('#loader' + name), true);
+
+        var json = JSON.stringify(viewModel.get('document'));
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/OBKCertificateReference/DocumentUpdate',
+            data: json,
+            contentType: 'application/json; charset=utf-8',
+            success: function (result) {
+                console.log("Файл успешно сохранен!");
+            },
+            complete: function () {
+                kendo.ui.progress($('#loader' + name), false);
+            }
+        });
+    };
+    loadDocument();
+
+    $('.k-grid-update').on('click', function () {
+        setGuid(docId);
+        sendData();
+    });
+}
+
+function InitializePropertyCertificate(name, viewModel) {
+    var dataOutgoingType = [
+       { text: "Инициативный", value: "0" },
+       { text: "Ответный", value: "1" },
+       { text: "Промежуточный", value: "2" }
+    ];
+    var initialFiles = viewModel.get('document.AttachFiles');
+
+    $("#files" + name).kendoUpload({
+        localization: {
+            select: 'Выбрать файл...',
+            remove: '',
+            cancel: '',
+            headerStatusUploading: "Загрузка...",
+            headerStatusUploaded: "Загружено!"
+        },
+        // multiple: true,
+        async: {
+            saveUrl: "/Upload/save",
+            removeUrl: "/Upload/remove",
+            autoUpload: true
+        }
+        , upload: function (e) {
+            e.data = { documentId: viewModel.get('document.AttachPath') };
+        },
+        remove: function (e) {
+            e.data = { documentId: viewModel.get('document.AttachPath') };
+        },
+        complete: function (e) {
+            //console.log('.k-upload', $("#files" + name).closest(".k-upload").find("a"));
+            //var data =
+            //data[0].onclick = function() {
+            //    fileView(viewModel.get('document.Id'), data[0].getAttribute('fileName'));
+            //};
+            var files = $("#files" + name).closest(".k-upload").find("a");
+            $.each(files, function (i, file) {
+                file.onclick = function () {
+                    fileView(viewModel.get('document.AttachPath'), file.getAttribute('fileName'));
+                };
+            });
+            var filesButton = $("#files" + name).closest(".k-upload").find("button");
+
+            $.each(filesButton, function (i, file) {
+                if (file.className == 'file-edit') {
+                    file.onclick = function () {
+                        fileEdit(viewModel.get('document.AttachPath'), file.getAttribute('fileName'));
+
+                    };
+                };
+            });
+            var filesDownload = $("#files" + name).closest(".k-upload").find("button");
+            $.each(filesDownload, function (i, file) {
+                if (file.className == 'file-download') {
+                    file.onclick = function () {
+                        fileDownload(viewModel.get('document.AttachPath'), file.getAttribute('fileName'));
+                    };
+                };
+            });
+            // e.files[0]["documentId"] = viewModel.get('document.Id');
+        },
+        select: function (e) {
+            // console.log('.k-upload', $("#files" + name).closest(".k-upload").find("a"));
+
+            //var files = e.files;
+            //$.each(files, function (i, file) {
+            //    file["documentId"] = viewModel.get('document.Id');
+            //    file.rawFile["documentId"] = viewModel.get('document.Id');
+            //    //$('span.k-filename[title="' + file.name + '"]').parent().remove();
+            //});
+            // e.files[0]["documentId"] = viewModel.get('document.Id');
+        },
+        template: kendo.template($('#fileTemplate').html()),
+        files: initialFiles
+    });
+}
