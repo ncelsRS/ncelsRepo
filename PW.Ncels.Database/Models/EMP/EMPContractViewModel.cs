f1359da18c08a35a9b95c4ceb5a24f0609a2ff9c
arman.s <armanoid.s.b@gmail.com>
2017-11-23 11:04:12 +0600

    Пропущенные файлы

---
+++ PW.Ncels.Database/Models/EMP/EMPContractViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW.Ncels.Database.Models.EMP
+{
+    public class EMPContractViewModel
+    {
+        public Guid Id { get; set; }
+        //public Guid? HolderType { get; set; }
+        public string MedicalDeviceName { get; set; }
+        public Declarations Manufactur { get; set; }
+        public Declarations Declarant { get; set; }
+        public Declarations Payer { get; set; }
+        public List<ServicePrices> ServicePrice { get; set; }
+    }
+}
---
+++ PW.Ncels.Database/Models/EMP/ServicePrices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW.Ncels.Database.Models.EMP
+{
+    public class ServicePrices
+    {
+        public Guid Id { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+        public decimal TotalCount { get; set; }
+        public string ServiceTypeNameRu { get; set; }
+        public string ServiceTypeNameKz { get; set; }
+    }
+}
