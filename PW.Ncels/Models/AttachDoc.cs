dc3251400eef1935fad223ef06c8803b1ef7ea6f
abzal.y <abbzik@mail.ru>
2017-11-29 13:30:50 +0600

    Letter with edo action views models

---
+++ PW.Ncels/Models/AttachDoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PW.Ncels.Models
+{
+    public class AttachDoc
+    {
+        public long ID { get; set; }
+        public string AttachmentName { get; set; }
+        public Nullable<long> LetterId { get; set; }
+        public string SignXml { get; set; }
+        public bool isSigned { get; set; }
+    }
+}
---
+++ PW.Ncels/Models/LetterModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PW.Ncels.Models
+{
+    public class LetterModel
+    {
+        public long ID { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public Nullable<System.DateTime> LetterSentEdoDate { get; set; }
+        public Nullable<System.Guid> AuthorID { get; set; }
+        public Nullable<System.DateTime> LetterRegDate { get; set; }
+        public string LetterContent { get; set; }
+        public Nullable<int> LetterStatusId { get; set; }
+        public Nullable<System.Guid> ContractId { get; set; }
+        public string EdoRegNomer { get; set; }
+        public Nullable<System.DateTime> EdoRegDate { get; set; }
+
+        public string contractName { get; set; }
+        public string nomerPisma { get; set; }
+
+        public string RegName { get; set; }
+
+        public List<AttachDoc> listDoc { get; set; }
+
+        public HttpPostedFileBase[] files { get; set; }
+    }
+}
