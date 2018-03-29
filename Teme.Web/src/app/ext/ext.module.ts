25186bea46b8999e42e9cd63080ab38fc1452960
Agyn <agyn.bolatov.1992@gmail.com>
2018-03-29 17:32:15 +0600

    header шаблон

--- Teme.Web/src/app/ext/ext.module.ts
+++ Teme.Web/src/app/ext/ext.module.ts
@@ -3,16 +3,18 @@ import {CommonModule} from '@angular/common';
 import {ExtLayoutComponent} from './ext-layout/ext-layout.component';
 import {routing} from './ext.routing';
 import { TestComponent } from './ext-layout/test/test.component';
+import { ExtUpHeaderComponent } from './ext-layout/ext-up-header/ext-up-header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     routing
   ],
-  declarations: [ExtLayoutComponent, TestComponent],
+  declarations: [ExtLayoutComponent, TestComponent, ExtUpHeaderComponent],
   exports: [
-    ExtLayoutComponent
-  ]
+    ExtLayoutComponent, ExtUpHeaderComponent
+  ],
+  bootstrap: [ExtLayoutComponent, ExtUpHeaderComponent]
 })
 export class ExtModule {
 }
