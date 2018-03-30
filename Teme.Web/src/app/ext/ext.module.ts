42971dffc31062f7d96f58c996ccbbcc7f5c16c2
Agyn <agyn.bolatov.1992@gmail.com>
2018-03-30 15:23:28 +0600

    Верстка шаблона внешнего портала header, menu

--- Teme.Web/src/app/ext/ext.module.ts
+++ Teme.Web/src/app/ext/ext.module.ts
@@ -4,13 +4,14 @@ import {ExtLayoutComponent} from './ext-layout/ext-layout.component';
 import {routing} from './ext.routing';
 import { TestComponent } from './ext-layout/test/test.component';
 import { ExtUpHeaderComponent } from './ext-layout/ext-up-header/ext-up-header.component';
+import { ExpMenuComponent } from './ext-layout/exp-menu/exp-menu.component';
 
 @NgModule({
   imports: [
     CommonModule,
     routing
   ],
-  declarations: [ExtLayoutComponent, TestComponent, ExtUpHeaderComponent],
+  declarations: [ExtLayoutComponent, TestComponent, ExtUpHeaderComponent, ExpMenuComponent],
   exports: [
     ExtLayoutComponent, ExtUpHeaderComponent
   ],
