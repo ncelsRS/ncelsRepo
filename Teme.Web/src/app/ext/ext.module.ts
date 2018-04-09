d59a055ed923c38b937a05ed04d75ebd79c060ad
Иван <ivan01march@gmail.com>
2018-04-09 16:03:03 +0600

    routings in ext portal

--- Teme.Web/src/app/ext/ext.module.ts
+++ Teme.Web/src/app/ext/ext.module.ts
@@ -2,11 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ExtLayoutComponent} from './ext-layout/ext-layout.component';
 import {routing} from './ext.routing';
-import {TestComponent} from './ext-layout/test/test.component';
 import {ExtUpHeaderComponent} from './ext-layout/ext-up-header/ext-up-header.component';
 import {ExpMenuComponent} from './ext-layout/exp-menu/exp-menu.component';
-import {ExtLayoutBodyComponent} from './ext-layout/ext-layout-body/ext-layout-body.component';
 import {ExtLayoutFooterComponent} from './ext-layout/ext-layout-footer/ext-layout-footer.component';
+import {ExtLayoutHomeComponent} from './ext-layout-pages/ext-layout-home/ext-layout-home.component';
 
 @NgModule({
   imports: [
@@ -15,16 +14,16 @@ import {ExtLayoutFooterComponent} from './ext-layout/ext-layout-footer/ext-layou
   ],
   declarations: [
     ExtLayoutComponent,
-    TestComponent,
     ExtUpHeaderComponent,
     ExpMenuComponent,
-    ExtLayoutBodyComponent,
-    ExtLayoutFooterComponent
+    ExtLayoutFooterComponent,
+    ExtLayoutHomeComponent
   ],
   exports: [
-    ExtLayoutComponent, ExtUpHeaderComponent, ExtLayoutBodyComponent, ExtLayoutFooterComponent
-  ],
-  bootstrap: [ExtLayoutComponent, ExtUpHeaderComponent, ExtLayoutBodyComponent, ExtLayoutFooterComponent]
+    ExtLayoutComponent,
+    ExtUpHeaderComponent,
+    ExtLayoutFooterComponent
+  ]
 })
 export class ExtModule {
 }
