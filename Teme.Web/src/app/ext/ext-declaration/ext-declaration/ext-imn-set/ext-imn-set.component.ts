a9c9a542a7c72700b9e0c441b9b961cf89b277e4
Agyn <agyn.bolatov.1992@gmail.com>
2018-04-10 14:39:25 +0600

    fix

---
+++ Teme.Web/src/app/ext/ext-declaration/ext-declaration/ext-imn-set/ext-imn-set.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ext-imn-set',
+  templateUrl: './ext-imn-set.component.html',
+  styleUrls: ['./ext-imn-set.component.css']
+})
+export class ExtImnSetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
