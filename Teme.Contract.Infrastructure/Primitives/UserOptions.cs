e44e0fdfc68746655df5633eda45327e8315f21d
Иван <ivan01march@gmail.com>
2018-04-17 12:33:02 +0600

    commit

---
+++ Teme.Contract.Infrastructure/Primitives/UserOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teme.Contract.Infrastructure.Primitives
+{
+    public class UserOptions
+    {
+        /// <summary>
+        /// Отправить с подписью
+        /// </summary>
+        public const string SendWithSign = "sendWithSign";
+        /// <summary>
+        /// Отпарвить без подписи
+        /// </summary>
+        public const string SendWithoutSign = "sendWithoutSign";
+
+    }
+}
