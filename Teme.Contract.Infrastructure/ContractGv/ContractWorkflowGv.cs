6bc510531ef7a3ada095f1cba826aff2668a9e72
Иван <ivan01march@gmail.com>
2018-04-18 15:02:38 +0600

    gitignore

--- Teme.Contract.Infrastructure/ContractGv/ContractWorkflowGv.cs
+++ Teme.Contract.Infrastructure/ContractGv/ContractWorkflowGv.cs
@@ -14,8 +14,8 @@ namespace Teme.Contract.Infrastructure.ContractGv
             builder.StartWith(c => Log.Information("StartWithGv"))
                 .ForEach(d => d.ExecutorsIds.ToList().First(x => x.Key == "forGv").Value)
                     .Do(t => t.StartWith(c => Log.Information("ForEach"))
-                        .UserTask("ChoiseExecutors", (d, c) => d.ExecutorsIds.ToList().First(x => x.Key == c.Item as string).Value.First())
-                            .WithOption(UserOptions.SelectExecutors, "o1").Do(c => Log.Information("EndGv"))
+                        //.UserTask("ChoiseExecutors", (d, c) => d.ExecutorsIds.ToList().First(x => x.Key == c.Item as string).Value.First())
+                        //    .WithOption(UserOptions.SelectExecutors, "o1").Do(c => Log.Information("EndGv"))
                     )
                 ;
 
