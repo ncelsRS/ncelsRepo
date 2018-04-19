41648e0d46efcfe28b22c6a60365855df3b8ae2e
Turlybek <turlybekzhumabaev@gmail.com>
2018-04-19 11:30:20 +0600

    Валидация

--- Teme.Web/src/app/ext/ext-contract/ext-contract/ext-contract.component.ts
+++ Teme.Web/src/app/ext/ext-contract/ext-contract/ext-contract.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit , Output, EventEmitter } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {RegisterType} from './RegisterType'
+import {ExtManufacturerComponent} from './ext-manufacturer/ext-manufacturer.component';
+import {ExtDeclarantComponent} from './ext-declarant/ext-declarant.component';
+import {ExtPayerComponent} from './ext-payer/ext-payer.component';
 
 @Component({
   selector: 'app-ext-contract',
@@ -10,12 +13,15 @@ import {RegisterType} from './RegisterType'
 })
 export class ExtContractComponent implements OnInit {
   selectedLevel: string;
+  public showAllErr = false;
   @Output() selectedLevelChange = new EventEmitter<string>();
   levels: Array<RegisterType> = [
     {code: 'Registration', name: 'Регистрация'},
     {code: 'Reregistration', name: 'Перерегистрация'},
     {code: 'Edit', name: 'Внесение изменений'},
   ];
+
+
   type: string;
   public id: string;
 
@@ -43,6 +49,48 @@ export class ExtContractComponent implements OnInit {
       .subscribe(params => {
         this.id = params.id;
       });
+  }
+
+  public contract: any = {
+    manufactur: {manufacturNameKz: null, manufacturNameRu: null,
+      manufacturAddressLegalRu: null, manufacturAddressFact: null,
+      manufacturPhone: null, manufacturEmail: null, manufacturNameEn: null,
+      manufacturCountry:null, manufacturOrgForm: null, bin: null, manufacturBossLastName: null,
+      manufacturBossFirstName: null, manufacturBossPosition: null, manufacturBossPositionKz: null, manufacturBankName: null,
+      manufacturBankIik: null, manufacturCurr: null, bankBik: null, manufacturNoResCountry:null, manufacturNoResName:null,
+
+    },
+
+    declarant: {
+      declarantBin: null, declarantNoResCounty: null, declarantRuName: null,
+      declarantOrgForm:null, declarantNameKz: null, declarantNameRu:null,declarantNameEn: null, declarantCountry:null,
+      declarantAddressLegalRu:null, declarantAddressFact:null,declarantPhone: null,declarantPhone2:null, declarantEmail:null,declarantBossLastName:null,
+      declarantBossFirstName:null,declarantBossPosition:null,declarantBossPositionKz:null,declarantBank:null,declarantBankIik:null,declarantBankCurr:null,declarantBankSwift:null,
+      },
+
+    payer: {payerBin: null, payerNoRes:null,payerNoResName:null,payerOrgForm:null,payerCountry:null,payerAddressLegalRu:null,payerBankName:null,
+      payerBankIik:null, payerBankSwift:null,payerBankCurr:null,
+    },
+
+    cost: {val6: null},
+  };
+
+
+  @ViewChild(ExtManufacturerComponent)
+  private ExtManufactur: ExtManufacturerComponent;
+
+  @ViewChild(ExtDeclarantComponent)
+  private ExtDeclarant: ExtDeclarantComponent;
+
+  @ViewChild(ExtPayerComponent)
+  private ExtPayer: ExtPayerComponent;
+
+  sendToNcels(valid) {
+    this.showAllErr = true;
+  }
+
+  diagnostic() {
+    return JSON.stringify(this.contract);
   }
 
 }
--- Teme.Web/src/app/ext/ext-contract/ext-contract/ext-manufacturer/ext-manufacturer.component.ts
+++ Teme.Web/src/app/ext/ext-contract/ext-contract/ext-manufacturer/ext-manufacturer.component.ts
@@ -1,18 +1,70 @@
-import { Component, OnInit, Input} from '@angular/core';
-import {RegisterType} from '../RegisterType';
+import {Component,   Input,  forwardRef, ViewChild} from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator
+} from "@angular/forms";
 
 @Component({
   selector: 'app-ext-manufacturer',
   templateUrl: './ext-manufacturer.component.html',
-  styleUrls: ['./ext-manufacturer.component.css']
+  styleUrls: ['./ext-manufacturer.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => ExtManufacturerComponent),
+    multi: true
+  }, {
+    provide: NG_VALIDATORS,
+    useExisting: forwardRef(() => ExtManufacturerComponent),
+    multi: true
+  }]
 })
-export class ExtManufacturerComponent implements OnInit {
-
+export class ExtManufacturerComponent implements  ControlValueAccessor, Validator{
+   private _model: any = {};
   @Input() prnRegisterType: string;
+   @ViewChild('ManufacturerForm') form;
+   @Input() showErrors = false;
 
-
-
-  ngOnInit() {
+  get model() {
+    return this._model;
   }
+
+  set model(v) {
+    this._model = v;
+    this.change(v);
+  }
+
+  private change = _ => {
+  };
+  private touch = () => {
+  };
+
+  registerOnChange(fn: any): void {
+    this.change = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.touch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+  }
+
+  writeValue(obj: any): void {
+    this.model = obj;
+    this.change(obj);
+  }
+
+  registerOnValidatorChange(fn: () => void): void {
+  }
+
+  validate(c: AbstractControl): ValidationErrors | null {
+    if (this.form.valid) return null;
+    return {error: true};
+  }
+
 
 }
