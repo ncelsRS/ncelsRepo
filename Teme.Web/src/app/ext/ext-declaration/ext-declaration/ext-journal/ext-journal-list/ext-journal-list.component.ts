61ec802919d8bbbfb5b89ea9058b555d3a12e9c2
Agyn <agyn.bolatov.1992@gmail.com>
2018-04-19 15:47:28 +0600

    Журнал образцов

---
+++ Teme.Web/src/app/ext/ext-declaration/ext-declaration/ext-journal/ext-journal-list/ext-journal-list.component.ts
@@ -0,0 +1,115 @@
+import {
+  Component, OnInit, ViewEncapsulation
+} from '@angular/core';
+
+@Component({
+  selector: 'app-ext-journal-list',
+  templateUrl: './ext-journal-list.component.html',
+  styleUrls: ['./ext-journal-list.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ExtJournalListComponent implements OnInit {
+  public registerSettings = {
+    selectMode: 'single',  //single|multi
+    hideHeader: false,
+    hideSubHeader: false,
+    actions: {
+      columnTitle: 'Действия',
+      add: true,
+      edit: true,
+      delete: true,
+      custom: [],
+      position: 'right' // left|right
+    },
+    add: {
+      addButtonContent: '<h4 class="mb-1"><i class="fa fa-plus ml-3 text-success"></i></h4>',
+      createButtonContent: '<i class="fa fa-check mr-3 text-success"></i>',
+      cancelButtonContent: '<i class="fa fa-times text-danger"></i>'
+    },
+    edit: {
+      editButtonContent: '<i class="fa fa-pencil mr-3 text-primary"></i>',
+      saveButtonContent: '<i class="fa fa-check mr-3 text-success"></i>',
+      cancelButtonContent: '<i class="fa fa-times text-danger"></i>'
+    },
+    delete: {
+      deleteButtonContent: '<i class="fa fa-trash-o text-danger"></i>',
+      confirmDelete: true
+    },
+    noDataMessage: 'Нет данных',
+    columns: {
+      additional: {
+        title: 'Дополнительно',
+        type: 'string',
+        filter: true
+      },
+      materialType: {
+        title: 'Тип материала',
+        type: 'string'
+      },
+      productName: {
+        title: 'Наименование изделия\\материала\n',
+        type: 'string'
+      },
+      quantity: {
+        title: 'Кол-во',
+        type: 'string'
+      },
+      measure: {
+        title: 'Ед.изм.' +
+        '\n',
+        type: 'string'
+      },
+      series: {
+        title: 'Серия' +
+        '\n',
+        type: 'string'
+      },
+      issueDate: {
+        title: 'Дата изготовления',
+        type: 'string'
+      },
+      term: {
+        title: 'Срок годности',
+        type: 'string'
+      }
+
+    },
+    pager: {
+      display: true,
+      perPage: 10
+    }
+  };
+  public data = [];
+  _ref:any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  removeObject(){
+    this._ref.destroy();
+  }
+
+  public onRowSelect(event) {
+    // console.log(event);
+  }
+
+  public onUserRowSelect(event) {
+    //console.log(event);   //this select return only one page rows
+  }
+
+  public onRowHover(event) {
+    //console.log(event);
+  }
+
+  public onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+}
--- Teme.Web/src/app/ext/ext-declaration/ext-declaration/ext-journal/ext-journal.component.ts
+++ Teme.Web/src/app/ext/ext-declaration/ext-declaration/ext-journal/ext-journal.component.ts
@@ -1,4 +1,8 @@
-import {Component, forwardRef, Input, ViewChild} from '@angular/core';
+import {
+  Component, forwardRef, Input, ViewChild,
+  ComponentFactoryResolver,
+  ViewContainerRef
+} from '@angular/core';
 import {
   AbstractControl,
   ControlValueAccessor,
@@ -7,6 +11,7 @@ import {
   ValidationErrors,
   Validator
 } from '@angular/forms';
+import { ExtJournalListComponent } from './ext-journal-list/ext-journal-list.component'
 
 @Component({
   selector: 'app-ext-journal',
@@ -25,80 +30,12 @@ import {
 export class ExtJournalComponent implements ControlValueAccessor, Validator {
   @Input() showErrors = false;
   @ViewChild('journalForm') form;
+  @ViewChild('parent', { read: ViewContainerRef }) container: ViewContainerRef;
   private _model: any = {};
   public data = [];
   showAgree: boolean;
-  public registerSettings = {
-    selectMode: 'single',  //single|multi
-    hideHeader: false,
-    hideSubHeader: false,
-    actions: {
-      columnTitle: 'Действия',
-      add: true,
-      edit: true,
-      delete: true,
-      custom: [],
-      position: 'right' // left|right
-    },
-    add: {
-      addButtonContent: '<h4 class="mb-1"><i class="fa fa-plus ml-3 text-success"></i></h4>',
-      createButtonContent: '<i class="fa fa-check mr-3 text-success"></i>',
-      cancelButtonContent: '<i class="fa fa-times text-danger"></i>'
-    },
-    edit: {
-      editButtonContent: '<i class="fa fa-pencil mr-3 text-primary"></i>',
-      saveButtonContent: '<i class="fa fa-check mr-3 text-success"></i>',
-      cancelButtonContent: '<i class="fa fa-times text-danger"></i>'
-    },
-    delete: {
-      deleteButtonContent: '<i class="fa fa-trash-o text-danger"></i>',
-      confirmDelete: true
-    },
-    noDataMessage: 'Нет данных',
-    columns: {
-      additional: {
-        title: 'Дополнительно',
-        type: 'string',
-        filter: true
-      },
-      materialType: {
-        title: 'Тип материала',
-        type: 'string'
-      },
-      productName: {
-        title: 'Наименование изделия\\материала\n',
-        type: 'string'
-      },
-      quantity: {
-        title: 'Кол-во',
-        type: 'string'
-      },
-      measure: {
-        title: 'Ед.изм.' +
-        '\n',
-        type: 'string'
-      },
-      series: {
-        title: 'Серия' +
-        '\n',
-        type: 'string'
-      },
-      issueDate: {
-        title: 'Дата изготовления',
-        type: 'string'
-      },
-      term: {
-        title: 'Срок годности',
-        type: 'string'
-      }
 
-    },
-    pager: {
-      display: true,
-      perPage: 10
-    }
-  };
-  constructor() {
+  constructor(private _cfr: ComponentFactoryResolver) {
     this.showAgree = false;
   }
 
@@ -109,14 +46,6 @@ export class ExtJournalComponent implements ControlValueAccessor, Validator {
       data(JSON.parse(req.response));
     };
     req.send();
-  }
-
-  public onDeleteConfirm(event): void {
-    if (window.confirm('Are you sure you want to delete?')) {
-      event.confirm.resolve();
-    } else {
-      event.confirm.reject();
-    }
   }
 
   get model() {
@@ -161,20 +90,13 @@ export class ExtJournalComponent implements ControlValueAccessor, Validator {
     this.showAgree = show;
   }
 
-  public onRowSelect(event) {
-    // console.log(event);
-  }
-
-  public onUserRowSelect(event) {
-    //console.log(event);   //this select return only one page rows
-  }
-
-  public onRowHover(event) {
-    //console.log(event);
-  }
-
-
   ngOnInit() {
+  }
+
+  addComponent(){
+    var comp = this._cfr.resolveComponentFactory(ExtJournalListComponent);
+    var expComponent = this.container.createComponent(comp);
+    expComponent.instance._ref = expComponent;
   }
 
 }
