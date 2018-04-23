c27dbc5a0a9e0c16f3acf369cfab24be8e973d88
Turlybek <turlybekzhumabaev@gmail.com>
2018-04-23 19:10:26 +0600

    Reference

--- Teme.Web/src/app/ext/ext-contract/ext-contract/ext-manufacturer/ext-manufacturer.component.ts
+++ Teme.Web/src/app/ext/ext-contract/ext-contract/ext-manufacturer/ext-manufacturer.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, forwardRef, ViewChild, Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
 import {
   AbstractControl,
   ControlValueAccessor,
@@ -9,6 +10,7 @@ import {
 } from "@angular/forms";
 import {TemplateValidation} from '../../../../shared/TemplateValidation';
 import {RefService} from '../ext-ref-sevice';
+import {DeclarantDocType} from '../ext-declarant/DeclarantDocType';
 
 @Component({
   selector: 'app-ext-manufacturer',
@@ -25,10 +27,15 @@ import {RefService} from '../ext-ref-sevice';
   }, RefService]
 })
 @Injectable()
-export class ExtManufacturerComponent extends TemplateValidation{
+export class ExtManufacturerComponent extends TemplateValidation {
 
    isAddOrgForm = false;
    isAddBankName = false;
+
+  selectedOrgForm: string;
+  public orgFormData ;
+  public orgFormData2=[] ;
+
   //private http: HttpClient;
 
 
@@ -36,8 +43,6 @@ export class ExtManufacturerComponent extends TemplateValidation{
    @Input() showErrors = false;
   constructor(private refService: RefService){
     super();
-   // this.getOrgForm();
-
   }
 
 
@@ -46,10 +51,10 @@ export class ExtManufacturerComponent extends TemplateValidation{
     this.isAddOrgForm = true;
   }
 
-  saveOrgForm()
-  {
-    this.isAddOrgForm = false;
-  }
+  // saveOrgForm()
+  // {
+  //
+  // }
 
   addBankName()
   {
@@ -64,8 +69,26 @@ export class ExtManufacturerComponent extends TemplateValidation{
   private url = "http://10.20.44.57:81/api/Reference/SaveOrganizationForm";
   //constructor(private http: HttpClient){ }
 
-  getOrgForm(){
-    this.refService.saveOrgForm();
+  saveOrgForm(nameKz:string,nameRu:string){
+    this.refService.saveManufacturOrgForm(nameKz,nameRu);
+    this.isAddOrgForm = false;
+  }
+
+  // getOrgForm(){
+  //   // this.OrgFormlevels =
+  //   console.log( this.refService.getManufacturOrgForm());
+  // }
+
+  getOrgForm() {
+       this.refService.getManufacturOrgForm().subscribe(
+            data=> { this.orgFormData = data; console.log(data)},
+          err => console.error(err),
+            () => console.log('done loading orgForm')
+        );
+       //this.orgFormData2 = JSON..parse(this.orgFormData);
+       //this.orgFormData2.push("id: 1, code: null, nameRu: 'Best', nameKz: 'Best'");
+       //console.log(this.orgFormData2);
+
   }
 
 
