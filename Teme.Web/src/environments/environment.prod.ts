a7e8b1debf179efea79fa0974f377a3b40721777
bekbol-ibragimov <bekbol.ibragimov@gmail.com>
2018-05-04 16:55:02 +0600

    payment urls

--- Teme.Web/src/environments/environment.prod.ts
+++ Teme.Web/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   urls: {
     identity: 'http://localhost:9433',
     extContract: 'http://localhost:9101',
-    admin: 'http://localhost:9121'
+    admin: 'http://localhost:9121',
+    payment: 'http://localhost:60825'
   }
 };
